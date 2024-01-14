de6c1d58e58228498e75a92e79391a02d4dea280
Dr.Ddiezel93 <ghek93@mail.ru>
2024-01-14 20:01:48 +0300

    Add homeWork seminar 2

---
+++ HW_Sem2/src/main/java/com/example/HW_Sem2/HwSem2Application.java
@@ -0,0 +1,13 @@
+package com.example.HW_Sem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSem2Application.class, args);
+	}
+
+}
